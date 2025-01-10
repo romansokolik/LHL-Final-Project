790bb271dd3cfb588738b149c19bd1fffcfb2bbd
Roman Sokolik <roman@sokolik.ca>
2025-01-10 10:03:45 -0500

    Add initial Django and DRF setup with basic API structure
    Set up a Django project with Django REST Framework integration, including serializers, views, and URL routing for a sample model. Added a basic index view and provided setup instructions in `setup-info.txt` to guide the development process.

---
+++ backend/backend/views.py
@@ -0,0 +1,7 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+
+def index(request):
+    # return render(request, 'frontend/index.html')
+    return HttpResponse("Hello, world. You're at the project (backend) index.")
