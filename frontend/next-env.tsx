68fb978ae2a6356857842073c1735e53b5afd2a9
Roman Sokolik <roman@sokolik.ca>
2025-01-28 19:14:08 -0500

    Add TypeScript declarations for Bootstrap JS imports
    Created module declarations for Bootstrap JS files to resolve TypeScript errors. Adjusted import statement in BootstrapScripts to align with declaration changes.

---
+++ frontend/next-env.tsx
@@ -0,0 +1,8 @@
+declare module 'bootstrap/dist/js/bootstrap.min.js' {
+    // const value: never;
+    // export default value;
+}
+declare module 'bootstrap/dist/js/bootstrap.bundle.js' {
+    // const value: never;
+    // export default value;
+}
--- frontend/src/components/BootstrapScripts.tsx
+++ frontend/src/components/BootstrapScripts.tsx
@@ -4,7 +4,7 @@ import {useEffect} from "react";
 export default function BootstrapScripts() {
 
     useEffect(() => {
-        import("bootstrap/dist/js/bootstrap.min.js");
+        import('bootstrap/dist/js/bootstrap.min.js');
         // import("bootstrap/dist/js/bootstrap.bundle.js");
     }, []);
 
